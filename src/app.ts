a51fbe8b1a6d4e5e53cf621f23117197e982fcc4
jorgemc94 <jmaciasc1994@gmail.com>
2024-07-24 09:36:10 +0200

    fix error mustache

--- src/app.ts
+++ src/app.ts
@@ -1,23 +1,25 @@
-import Express, { Response, Request, NextFunction } from 'express';
+import express, { Response, Request, NextFunction } from 'express';
 import { BookingController } from './controllers/booking';
 import { ContactController } from './controllers/contact';
 import { RoomController } from './controllers/room';
 import { UserController } from './controllers/user';
 import { ErrorApi } from './utils/error';
 import path from 'path';
-import mustacheExpress from 'mustache-express'
+import mustacheExpress from 'mustache-express';
 
-export const app = Express();
+export const app = express();
 export const port = 3000;
-app.use(Express.json())
 
-app.use(Express.static(path.join(__dirname, 'public')));
-app.set('views', './views');
+app.use(express.json());
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'mustache');
 app.engine('mustache', mustacheExpress());
-app.get('/public', (_req, res) => {
+
+app.get('/', (_req, res) => {
     res.render('index');
-  })
+});
 
 app.use('/bookings', BookingController);
 app.use('/contacts', ContactController);
@@ -25,6 +27,7 @@ app.use('/rooms', RoomController);
 app.use('/users', UserController);
 
 app.use((error: ErrorApi, _req: Request, res: Response, _next: NextFunction) => {
-    res.status(error.status || 500).json({message: error.safe ? error.message : "Error in the application"})
+    res.status(error.status || 500).json({
+        message: error.safe ? error.message : 'Error in the application',
+    });
 });
-
